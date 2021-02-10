581d89df5e522c4ceacbf0db3a8f5867ee1c3d01
josenetoreis <57234533+josenetoreis@users.noreply.github.com>
2021-02-10 00:50:03 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from calculadora import Calculadora
+from comodo import Comodo
+
+calc = Calculadora()
+
+comodo = Comodo(
+      input('Qual a largura do cômodo? '),
+      input('Qual a profundidade do cômodo?')
+)
+
+
+print(" a área das paredes é:",
+      calc.calcular_area_paredes
+            (comodo)
+      )
+
+print('A área do teto é:',
+      calc.calcular_area_teto
+      (comodo)
+      )
+
+print(
+      "A litragem de tinta necessaria é:",
+      calc.calcular_litragem_necessaria()
+      )
